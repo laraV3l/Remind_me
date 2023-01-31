4fa048897edf38ddb20c88d6d869d47984b2c955
root <root@localhost.localdomain>
2023-01-31 22:08:31 +0000

    I have added the database file

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Remind(models.Model):
+    name = models.CharField(max_length=1000)
+    email = models.EmailField()
+    time = models.DateTimeField()
+    description = models.TextField(max_length=10000, default=' ')
+
+    def __str__(self):
+        return self.name
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Remind
+
 # Create your views here.
 
 def home(request):
     return render(request,'landing/index.html')
+
+def login(request):
+    pass
+
+def task(request):
+    return render(request, "landing/task.html",context={ 'take': Remind.objects.all()})
